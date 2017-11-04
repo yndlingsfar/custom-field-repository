0362fa5181c7a5ceda2d4b25cfa2b6ccdd8b8404
dsteiner23 <daniel.steiner@gmx.de>
2017-11-04 08:48:15 +0100

    temporatily working autoloader

--- acf-abstraction-layer.php
+++ acf-abstraction-layer.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: ACF Abstraction Layer
+Plugin Name: Custom Field Repository
 Plugin URI: https://danielsteiner-ws.de
 Description: Provides functionality for mapping ACF groups to objects
 Author: dsteiner23
@@ -22,9 +22,12 @@ function check_acf_dependecies(){
  *
  */
 
+include_once realpath( plugin_dir_path( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'libs' . DIRECTORY_SEPARATOR . 'annotations' . DIRECTORY_SEPARATOR . 'Annotations.php';
+
 spl_autoload_register( 'acf_abstraction_autoloader' );
 function acf_abstraction_autoloader( $class_name ) {
 	$classes_dir = realpath( plugin_dir_path( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
-	$class_file = $class_name  . '.php';
+	$class_file = str_replace('DSteiner23\Custom_Field_Repository\\', '', $class_name)  . '.php';
+	$class_file = str_replace('\\', '/', $class_file);
 	require_once $classes_dir . $class_file;
 }
