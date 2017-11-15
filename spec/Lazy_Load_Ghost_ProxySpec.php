7d3c1f7d5bafef60db3534b8c8030e63a7f358b0
dsteiner23 <daniel.steiner@gmx.de>
2017-11-15 20:42:54 +0100

    add specs

---
+++ spec/Lazy_Load_Ghost_ProxySpec.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace spec\DSteiner23\Custom_Field_Repository;
+
+use Alchemy\Component\Annotations\Annotations;
+use DSteiner23\Custom_Field_Repository\Lazy_Load_Ghost_Proxy;
+use DSteiner23\Custom_Field_Repository\Provider\Provider_Interface;
+use PhpSpec\ObjectBehavior;
+use Test\Fixtures\Annotation_Valid;
+
+class Lazy_Load_Ghost_ProxySpec extends ObjectBehavior
+{
+    function let(Provider_Interface $provider, Annotations $annotations)
+    {
+	    $annotations->getClassAnnotations( Annotation_Valid::class )->willReturn(
+		    [
+			    'Field_Group' => [
+				    0 => [
+					    'name' => 'some_field_group',
+					    'provider' => 'acf',
+				    ]
+			    ]
+		    ]
+	    );
+
+	    $annotations->getPropertyAnnotations(Annotation_Valid::class, 'report')->willReturn(
+		    [
+			    'Field' => [
+				    0 => [
+					    'name' => 'report',
+					    'type' => 'text',
+				    ]
+			    ]
+		    ]
+	    );
+
+	    $class = new Annotation_Valid();
+	    $class->set_report('the_report');
+
+		$this->beConstructedWith($provider, $annotations, $class, 1);
+		$this->add_change('some_property');
+    }
+
+	function it_is_initializable()
+    {
+        $this->shouldHaveType(Lazy_Load_Ghost_Proxy::class);
+    }
+
+    function it_should_check_if_property_changed()
+    {
+    	$this->is_changed('report')->shouldReturn(true);
+    }
+
+	function it_should_get_all_changes()
+	{
+		$this->get_changes()->shouldReturn(['some_property']);
+	}
+
+	function it_should_get_the_id()
+	{
+		$this->get_id()->shouldReturn(1);
+	}
+
+	function it_should_get_the_client()
+	{
+		$this->get_client()->shouldReturnAnInstanceOf(Provider_Interface::class);
+	}
+
+	function  it_should_get_the_property_name()
+	{
+		$this->get_property_path('report')->shouldReturn('some_field_group.report');
+	}
+
+	function it_should_get_the_property_value()
+	{
+		$this->get_property_value('report')->shouldReturn('the_report');
+	}
+}
